1dc5989d36347f7b800f7fb9780f36fc2e3ed864
Jason Chang <chang.jasonp@gmail.com>
2017-11-10 15:26:01 -0800

    Added a check for existence of data directory in scrape
    - now checks for existence of the directory for database
    - if directory doesn't exist, then create the directory
    - can handle subdirectories

--- src/scrape_pitchfx.py
+++ src/scrape_pitchfx.py
@@ -34,32 +34,35 @@ dbname = sys.argv[3]
 prompt = bool(sys.argv[4])
 
 # parse beginning and end dates
-date = re.compile("^(\d\d)-(\d\d)-(\d\d\d\d)$")                                 
-find = date.search(bdate)                                                       
-if (find):                                                                      
+date = re.compile("^(\d\d)-(\d\d)-(\d\d\d\d)$")       
+find = date.search(bdate)   
+if (find):     
     bmonth = find.group(1)
     bday = find.group(2)
     byear = find.group(3)
-else:                                                                           
+else:          
     print "Start date format does not work"
     sys.exit()
-bdate = int('%s%s%s' %(byear, bmonth, bday))                                      
-find = date.search(edate)                                                       
-if find:                                                                      
+bdate = int('%s%s%s' %(byear, bmonth, bday))            
+find = date.search(edate)   
+if find:     
     emonth = find.group(1)
     eday = find.group(2)
     eyear = find.group(3)
-else:                                                                           
+else:          
     print "End date format does not work"
     sys.exit()
-edate = int('%s%s%s' %(eyear, emonth, eday))                                      
-# check that dates make sense                                                   
-if edate < bdate:                                                             
+edate = int('%s%s%s' %(eyear, emonth, eday))            
+# check that dates make sense            
+if edate < bdate:         
     print "Need begin date to be earlier than end date"
     sys.exit()
 
 # touch sqlite3 database
 if os.path.isfile(dbname) is False:
+   dbdir = "/".join(dbname.split("/")[:-1])
+   if not os.path.isdir(dbdir):
+     os.makedirs(dbdir)
    db = sqlite3.connect(dbname)
    hdb = db.cursor()
    pm.pitchfx_init(hdb)
