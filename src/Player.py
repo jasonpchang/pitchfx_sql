0da78ddae88a85be303b2d995b95c53b5cb825cb
Jason Chang <chang.jasonp@gmail.com>
2017-11-30 11:34:09 -0800

    Added games and events to Player class

--- src/Player.py
+++ src/Player.py
@@ -12,6 +12,29 @@ class Player():
         """
         # parse name
         self.first, self.last = name.split(" ")
+        
+    def pitch_games(self, database):
+        """Grab all games from database player pitched in
+        
+        Inputs:
+            database: sqlite object of database to read from
+        
+        Outpus:
+            games: pandas dataframe containing games player played in
+        """
+        # grab all games
+        query = """SELECT DISTINCT games.*
+                FROM games
+                JOIN events ON (games.game_id=events.game_id)
+                WHERE events.pitcher_id=(SELECT player_id
+                FROM players
+                WHERE players.player_first='%s'
+                    AND players.player_last='%s')
+                ORDER BY games.game_id""" %(self.first, self.last)
+        self.player_pgames = pd.read_sql_query(query, database)
+        
+        # clean up
+        return self.player_pgames
 
     def pitches(self, database, **params):
         """Grab all pitches from database thrown by player
@@ -45,3 +68,25 @@ class Player():
         
         # clean up
         return self.player_pfx
+    
+    def pitch_events(self, database):
+        """Grab events where player is the pitcher
+        
+        Inputs:
+            database: sqlite object of database to read from
+            
+        Output:
+            events: events where player is the pitcher
+        """
+        # grab all events
+        query = """SELECT DISTINCT * 
+                FROM events
+                WHERE pitcher_id=(SELECT player_id
+                FROM players
+                WHERE players.player_first='Barry'
+                    AND players.player_last='Zito')
+                ORDER BY game_id, event_id"""
+        self.player_pevents = pd.read_sql_query(query, database)
+        
+        # clean up
+        return self.player_pevents
