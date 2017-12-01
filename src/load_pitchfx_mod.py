1effdd13067717819bbe17292ee97d29276d77c0
Jason Chang <chang.jasonp@gmail.com>
2017-12-01 15:17:13 -0800

    Fixed issue with counting outs during double plays
    - used INSERT OR REPLACE rather than INSERT OR IGNORE in events

--- src/load_pitchfx_mod.py
+++ src/load_pitchfx_mod.py
@@ -450,7 +450,6 @@ def pitchfx_add(db, hdb, date1, date2, prompt):
                         for inning in header:
                             # new inning flag
                             inning_flag = 1
-                            pre_outs = 0
                             post_outs = 0
                             pre_1b = 0
                             post_1b = 0
@@ -476,7 +475,7 @@ def pitchfx_add(db, hdb, date1, date2, prompt):
                                         action_event = edict['event']
                                         action_flag = 1
                                 elif etype.tag == 'atbat':
-                                    #pre_outs = post_outs
+                                    pre_outs = post_outs
                                     # flag for events with multiple runners
                                     runner_flag = 1
                                     # reset count at beginning of at-bat
@@ -648,7 +647,7 @@ def pitchfx_add(db, hdb, date1, date2, prompt):
                                         # fill in event table
                                         info = (game_id, event_id, event_description_ab, inning_num, is_top, pre_outs, end_of_ab_outs, pitcher_id, batter_id, pre_1b, post_1b, pre_2b, post_2b, pre_3b, post_3b, post_home, pre_home_score, post_home_score, pre_away_score, post_away_score)
                                         #info = (game_id, event_id, event_description_ab, inning_num, is_top, pre_outs, end_of_ab_outs, pitcher_id, batter_id, batter_id, 'H', '0', pre_home_score, post_home_score, pre_away_score, post_away_score)
-                                        insert = "INSERT OR IGNORE INTO events VALUES (" \
+                                        insert = "INSERT OR REPLACE INTO events VALUES (" \
                                             "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, " \
                                             "?, ?, ?, ?, ?, ?, ?, ?, ?, ?" \
                                             ")"                             
